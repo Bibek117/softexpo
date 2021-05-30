2d1cb96909c036bb923c974ed520661d4693888b
Gharti Sirjana <ghartisirjana8@gmail.com>
2021-05-30 08:17:50 +0545

    app setting create

---
+++ app/Http/Controllers/Api/AppSettingController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\AppSetting;
+use Illuminate\Http\Request;
+
+class AppSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data =  AppSetting::all();
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(AppSetting $appsetting)
+    {
+        AppSetting::create($appsetting->all());
+        return response()->json(['message' => 'Appsetting created successfully'],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AppSetting $appsetting)
+    {
+        $data = AppSetting::find($appsetting->id);
+        return response()->json($data, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AppSetting $appsetting)
+    {
+        AppSetting::find($appsetting->id)->update($request->all());
+        return response()->json(['message' => 'App Setting updated successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AppSetting $appsetting)
+    {
+        $asdelete = AppSetting::find($appsetting->id);
+        $asdelete->delete();
+        return response()->json(['message' => 'App setting deleted successfully'], 200);
+    }
+}
