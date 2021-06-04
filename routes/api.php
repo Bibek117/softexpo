938e2a11a0850bd796085ef10d582d570494e95c
Bibek_16 <71329649+Bibek117@users.noreply.github.com>
2021-06-04 13:37:15 +0545

    logo handle

--- routes/api.php
+++ routes/api.php
@@ -118,6 +118,40 @@ Route::group(['as' => 'api.'], function () {
     Route::delete('software_pricing/delete/{id}', [SoftwarepricingController::class, 'destroy']);
 
     // -------------- software pricing
+
+    
+    //mobilesoftwareios---------------------------------------------------------------------
+
+    Route::get('mobile',[App\Http\Controllers\Api\MobilesoftwareiosController::class,'index'])->name('show_mobile');
+    Route::post('mobile/store',[App\Http\Controllers\Api\MobilesoftwareiosController::class,'store'])->name('store_mobile');
+    Route::get('mobile/show/{id}',[App\Http\Controllers\Api\MobilesoftwareiosController::class,'show'])->name('show_mobile');
+    Route::put('mobile/update/{id}' , [\App\Http\Controllers\Api\MobilesoftwareiosController::class , 'update'])->name('update_mobile');
+
+    Route::delete('mobile/delete/{id}',[App\Http\Controllers\Api\MobilesoftwareiosController::class,'destroy'])->name('delete_mobile');
+ 
+    //.........................................................................................................mobilesoftwareios
+
+
+
+    //mobilesoftwareandroid------------------------------------------------------------------------------------------------
+    Route::get('android',[App\Http\Controllers\Api\MobilesoftwareandroidController::class,'index'])->name('show_android');
+    Route::post('android/store',[App\Http\Controllers\Api\MobilesoftwareandroidController::class,'store'])->name('store_android');
+    Route::get('android/show/{id}',[App\Http\Controllers\Api\MobilesoftwareandroidController::class,'show'])->name('show_android');
+    Route::put('android/update/{id}' , [\App\Http\Controllers\Api\MobilesoftwareandroidController::class , 'update'])->name('update_android');
+    Route::delete('android/delete/{id}',[App\Http\Controllers\Api\MobilesoftwareandroidController::class,'destroy'])->name('delete_android');
+
+    //--------------------------------------------------------------------------------------------------------mobilesoftwareandroid
+
+
+    //softwarecategory----------------------------------------------------------------------------------------------
+
+    Route::get('software',[App\Http\Controllers\Api\SoftwarecategoryController::class,'index'])->name('show_software');
+    Route::post('software/store',[App\Http\Controllers\Api\SoftwarecategoryController::class,'store'])->name('store_software');
+    Route::get('software/show/{id}',[App\Http\Controllers\Api\SoftwarecategoryController::class,'show'])->name('show_software');
+    Route::put('software/update/{id}' , [\App\Http\Controllers\Api\SoftwarecategoryController::class , 'update'])->name('update_software');
+    Route::delete('software/delete/{id}',[App\Http\Controllers\Api\SoftwarecategoryController::class,'destroy'])->name('delete_software');
+
+
  
 
 });
